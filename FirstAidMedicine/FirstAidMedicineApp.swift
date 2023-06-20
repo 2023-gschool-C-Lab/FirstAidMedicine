c171cd41a2fbc89d006f9ee20cff69fd432ea185
dev-J20NG <g20kimjeongpyo1211@gmail.com>
2023-06-20 11:33:09 +0900

    Initial Commit

---
+++ FirstAidMedicine/FirstAidMedicineApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAidMedicineApp.swift
+//  FirstAidMedicine
+//
+//  Created by 김정표 on 2023/06/20.
+//
+
+import SwiftUI
+
+@main
+struct FirstAidMedicineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
