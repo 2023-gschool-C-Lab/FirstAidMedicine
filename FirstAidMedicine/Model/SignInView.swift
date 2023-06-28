72b987f758376f408cdb261f0ddf7025badc198f
dev-J20NG <g20kimjeongpyo1211@gmail.com>
2023-06-28 12:28:26 +0900

    Reinforement SignInView , Drawing UI

--- FirstAidMedicine/Model/SignInView.swift
+++ FirstAidMedicine/Model/SignInView.swift
@@ -16,19 +16,42 @@ struct SignInView: View {
 
     var body: some View {
         VStack {
-            Form { // 사용자의 상호작용 뷰 컨테이너
+            VStack { // 사용자의 상호작용 뷰 컨테이너
                 TextField( // 매개변수와 옵션을 통해 사용쟈의 인터페이스 조성
                     "이메일을 입력해주세요.",
                     text : $email //  텍스트는 이메일이며, 이메일 값의 변경 상태를 추적하고 연동하겠다.
                 )
-                .textInputAutocapitalization(.never) // 텍스트가 자동으로 대문자화 되는 것을 막겠다.
+                .font(.system(size: 16))
+                               .fontWeight(.light)
+                               .textFieldStyle(PlainTextFieldStyle())
+                               .padding(.vertical)
+                               .padding(.leading,25)
+                               .background(
+                                   Capsule()
+                                       .strokeBorder(Color(hex: 0xD9D9D9), lineWidth: 1)
+                               )
+                               
+                               .textInputAutocapitalization(.never) // 텍스트가 자동으로 대문자화 되는 것을 막겠다.
                 
                 SecureField(
                     "비밀번호를 입력해주세요.",
                     text: $password //  텍스트는 패스워드이며, 패스워드의 값의 변경 상태를 추적하고 연동하겠다.
                 )
+                .font(.system(size: 16))
+                               .fontWeight(.light)
+                               .textFieldStyle(PlainTextFieldStyle())
+                               .padding(.vertical)
+                               .padding(.leading,25)
+                               .background(
+                                   Capsule()
+                                       .strokeBorder(Color(hex: 0xD9D9D9), lineWidth: 1)
+                               )
                 .textInputAutocapitalization(.never)  // 텍스트가 자동으로 대문자화 되는 것을 막겠다.
             }
+            .padding(30)
+            
+            Spacer()
+                .frame(maxHeight: 28)
             
             Button {
                 Task {
@@ -42,12 +65,15 @@ struct SignInView: View {
                 }
             } label: {
                 Text("로그인")
+                    .fontWeight(.medium)
+                    .font(.system(size: 23))
                     .foregroundColor(.white)
                     .padding()
-                    .frame(width: 150)
-                    .background(.blue)
-                    .cornerRadius(12)
+                    .frame(width: 333,height: 64)
+                    .background(Color(hex: 0x5762EA))
+                    .cornerRadius(36)
             }
+            
             
             Button {
                 authViewModel.loginState = .notSigned // 버튼을 누르면 로그인의 상태를 로그인 되지 않음으로 변경한다.
@@ -71,5 +97,6 @@ struct SignInView: View {
 struct SignInView_Previews: PreviewProvider {
     static var previews: some View {
         SignInView()
+            .environmentObject(AuthViewModel())
     }
 }
